10634c7ae4107f028b63db5777a0c7cea4e875e9
Lucrada <iamdracula.dracula@gmail.com>
2023-04-25 15:57:10 +0530

    card styling

--- client/src/components/common/HorizontalSlidingTile.tsx
+++ client/src/components/common/HorizontalSlidingTile.tsx
@@ -2,14 +2,42 @@ import '../../sass/components/horizontal_sliding_tile.scss';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import { Link } from 'react-router-dom';
+import { VerticalSpacer } from '../Helpers/VerticalSpacer';
 
-interface GameTile {
-    name: string,
+export interface Card {
+    title: string,
+    img: string,
+    desc: string,
     rating: number,
+    players: number,
 }
 
-export function HorizontalSlidingTile(props: { title: string, gameList: Array<GameTile>, link: string }) {
-    const { title, gameList, link } = props;
+function SlidingCard(props: { details: Card }) {
+    const { details } = props;
+
+    return (
+        <div className='sliding-tile-parent__slider-gameList--game'>
+            <img src={details.img} alt={details.title} />
+            <div className='sliding-tile-parent__slider-gameList--game__card-details'>
+                <div className='sliding-tile-parent__slider-gameList--game__card-details--header'>
+                    <h2>{details.title}</h2>
+                    <span>{details.rating}/5</span>
+                </div>
+                <VerticalSpacer rem={0.2} />
+                <div className='sliding-tile-parent__slider-gameList--game__card-details--info'>
+                    <span>Players: {details.players}</span>
+                </div>
+                <VerticalSpacer rem={0.2} />
+                <div className='sliding-tile-parent__slider-gameList--game__card-details--body'>
+                    <p>{details.desc}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export function HorizontalSlidingTile(props: { title: string, cardDetails: Array<Card>, link: string }) {
+    const { title, cardDetails, link } = props;
 
     return (
         <div className='sliding-tile-parent'>
@@ -20,7 +48,7 @@ export function HorizontalSlidingTile(props: { title: string, gameList: Array<Ga
             <div className='sliding-tile-parent__slider'>
                 <KeyboardArrowLeftIcon className='sliding-tile-parent__slider--left-slide' />
                 <div className="sliding-tile-parent__slider-gameList">
-                    {gameList.map(game => <div className='sliding-tile-parent__slider-gameList--game'></div>)}
+                    {cardDetails.map(card => <SlidingCard details={card} />)}
                 </div>
                 <KeyboardArrowRightIcon className='sliding-tile-parent__slider--right-slide' />
             </div>
--- client/src/pages/child/Home.tsx
+++ client/src/pages/child/Home.tsx
@@ -1,25 +1,24 @@
 import { VerticalSpacer } from '../../components/Helpers/VerticalSpacer';
-import { HorizontalSlidingTile } from '../../components/common/HorizontalSlidingTile';
+import { HorizontalSlidingTile, Card } from '../../components/common/HorizontalSlidingTile';
 import '../../sass/pages/home.scss';
 
 export default function Home() {
-    const gameList = [
-        { name: 'GTA', rating: 5 },
-        { name: 'GTA', rating: 5 },
-        { name: 'GTA', rating: 5 },
-        { name: 'GTA', rating: 5 },
-        { name: 'GTA', rating: 5 },
-        { name: 'GTA', rating: 5 },
+    const cardDetails: Array<Card> = [
+        { title: 'Call of Duty', img: `${process.env.PUBLIC_URL}/assets/test_images/background.jpg`, players: 500, rating: 5, desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque velit optio vel debitis mollitia commodi doloribus architecto dolore culpa consectetur vero corrupti sint, iste corporis sed distinctio modi minima recusandae.' },
+        { title: 'War fare', img: `${process.env.PUBLIC_URL}/assets/test_images/background2.jpg`, players: 450, rating: 4, desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque velit optio vel debitis mollitia commodi doloribus architecto dolore culpa consectetur vero corrupti sint, iste corporis sed distinctio modi minima recusandae.' },
+        { title: 'Cyberpunk', img: `${process.env.PUBLIC_URL}/assets/test_images/background3.jpg`, players: 120, rating: 1, desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque velit optio vel debitis mollitia commodi doloribus architecto dolore culpa consectetur vero corrupti sint, iste corporis sed distinctio modi minima recusandae.' },
+        { title: 'GTA V', img: `${process.env.PUBLIC_URL}/assets/test_images/background4.jpg`, players: 5000, rating: 2, desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque velit optio vel debitis mollitia commodi doloribus architecto dolore culpa consectetur vero corrupti sint, iste corporis sed distinctio modi minima recusandae.' },
+        { title: 'No mans sky', img: `${process.env.PUBLIC_URL}/assets/test_images/background5.jpg`, players: 300, rating: 3, desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque velit optio vel debitis mollitia commodi doloribus architecto dolore culpa consectetur vero corrupti sint, iste corporis sed distinctio modi minima recusandae.' },
     ];
 
     return (
         <section className='home-page'>
             <VerticalSpacer rem={2} />
-            <HorizontalSlidingTile title='Most Popular' gameList={gameList} link='/' />
+            <HorizontalSlidingTile title='Most Popular' cardDetails={cardDetails} link='/' />
             <VerticalSpacer rem={4} />
-            <HorizontalSlidingTile title='Top Rated' gameList={gameList} link='/' />
+            <HorizontalSlidingTile title='Top Rated' cardDetails={cardDetails} link='/' />
             <VerticalSpacer rem={4} />
-            <HorizontalSlidingTile title='Latest News' gameList={gameList} link='/' />
+            <HorizontalSlidingTile title='Latest News' cardDetails={cardDetails} link='/' />
             <VerticalSpacer rem={2} />
         </section>
     );
