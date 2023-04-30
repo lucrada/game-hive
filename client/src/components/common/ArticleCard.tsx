dac3ace823d5083141770a11e9514c57090c4cef
Lucrada <iamdracula.dracula@gmail.com>
2023-04-30 23:42:14 +0530

    added article page

---
+++ client/src/components/common/ArticleCard.tsx
@@ -0,0 +1,24 @@
+import '../../sass/components/article_card.scss';
+
+export function ArticleCard() {
+    return (
+        <div className="article-card">
+            <img className='article-card__thumbnail' src={`${process.env.PUBLIC_URL}/assets/test_images/background.jpg`} alt="thumbnail" />
+            <div className="article-card__content">
+                <div className="article-card__content--info">
+                    <span className='article-card__content--info__left'>
+                        By Seline Shenoy
+                    </span>
+                    <span className='article-card__content--info__right'>
+                        20-07-2023
+                    </span>
+                </div>
+                <h2 className="article-card__content--title">Is Social Media the bad guy? Redefining Beauty in a digital world</h2>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt aliquam laboriosam nostrum quae asperiores. Esse ea tenetur possimus ad, sapiente.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus, repellendus commodi. Qui impedit optio ipsa aliquam culpa accusantium, quod repellendus earum laboriosam autem tempora nemo repellat neque rerum reiciendis soluta.
+                </p>
+            </div>
+        </div>
+    );
+}
--- client/src/pages/child/Articles.tsx
+++ client/src/pages/child/Articles.tsx
@@ -1,9 +1,22 @@
+import { ArticleCard } from '../../components/common/ArticleCard';
 import '../../sass/pages/articles.scss';
 
 export default function Articles() {
     return (
         <section className='article-page'>
-
+            <div className="article-page__header">
+                <h1>Articles</h1>
+            </div>
+            <div className="article-page__body">
+                <div className="article-page__body--cards">
+                    <ArticleCard />
+                    <ArticleCard />
+                    <ArticleCard />
+                    <ArticleCard />
+                    <ArticleCard />
+                    <ArticleCard />
+                </div>
+            </div>
         </section>
     );
 }
