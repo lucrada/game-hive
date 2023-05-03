835ab13d49238eb98d26fad870a6246d685a949b
Lucrada <iamdracula.dracula@gmail.com>
2023-05-03 19:48:08 +0530

    refactored articles.tsx and news.tsx

--- client/src/components/common/ArticleCard.tsx
+++ client/src/components/common/ArticleCard.tsx
@@ -1,23 +1,28 @@
 import '../../sass/components/article_card.scss';
 
-export function ArticleCard(): JSX.Element {
+export interface Article {
+    imageURL: string,
+    author: string,
+    date: string,
+    title: string,
+    body: string,
+}
+
+export function ArticleCard(props: { article: Article }): JSX.Element {
     return (
         <div className="article-card">
-            <img className='article-card__thumbnail' src={`${process.env.PUBLIC_URL}/assets/test_images/background.jpg`} alt="thumbnail" />
+            {props.article.imageURL !== "" ? <img className='article-card__thumbnail' src={props.article.imageURL} alt="thumbnail" /> : null}
             <div className="article-card__content">
                 <div className="article-card__content--info">
                     <span className='article-card__content--info__left'>
-                        By Seline Shenoy
+                        By {props.article.author}
                     </span>
                     <span className='article-card__content--info__right'>
-                        20-07-2023
+                        {props.article.date}
                     </span>
                 </div>
-                <h2 className="article-card__content--title">Is Social Media the bad guy? Redefining Beauty in a digital world</h2>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt aliquam laboriosam nostrum quae asperiores. Esse ea tenetur possimus ad, sapiente.
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus, repellendus commodi. Qui impedit optio ipsa aliquam culpa accusantium, quod repellendus earum laboriosam autem tempora nemo repellat neque rerum reiciendis soluta.
-                </p>
+                <h2 className="article-card__content--title">{props.article.title}</h2>
+                <p>{props.article.body}</p>
             </div>
         </div>
     );
--- client/src/pages/child/Articles.tsx
+++ client/src/pages/child/Articles.tsx
@@ -1,7 +1,19 @@
 import { ArticleCard } from '../../components/common/ArticleCard';
 import '../../sass/pages/articles.scss';
+import { Article } from '../../components/common/ArticleCard';
 
 export default function Articles(): JSX.Element {
+
+    const articles: Array<Article> = [
+        {
+            imageURL: `${process.env.PUBLIC_URL}/assets/test_images/background.jpg`,
+            author: "Seline Shenoy",
+            date: "20-07-2023",
+            title: "Is Social Media the bad guy? Redefining Beauty in a digital world",
+            body: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt aliquam laboriosam nostrum quae asperiores. Esse ea tenetur possimus ad, sapiente.\nLorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus, repellendus commodi. Qui impedit optio ipsa aliquam culpa accusantium, quod repellendus earum laboriosam autem tempora nemo repellat neque rerum reiciendis soluta.",
+        }
+    ];
+
     return (
         <section className='article-page'>
             <div className="article-page__header">
@@ -9,12 +21,12 @@ export default function Articles(): JSX.Element {
             </div>
             <div className="article-page__body">
                 <div className="article-page__body--cards">
-                    <ArticleCard />
-                    <ArticleCard />
-                    <ArticleCard />
-                    <ArticleCard />
-                    <ArticleCard />
-                    <ArticleCard />
+                    <ArticleCard article={articles[0]} />
+                    <ArticleCard article={articles[0]} />
+                    <ArticleCard article={articles[0]} />
+                    <ArticleCard article={articles[0]} />
+                    <ArticleCard article={articles[0]} />
+                    <ArticleCard article={articles[0]} />
                 </div>
             </div>
         </section>
