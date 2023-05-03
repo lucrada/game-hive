a60ebbc9cad951814b666fc2364c08a5261c9c02
Lucrada <iamdracula.dracula@gmail.com>
2023-05-03 19:36:32 +0530

    added new page

--- client/src/pages/child/News.tsx
+++ client/src/pages/child/News.tsx
@@ -1,9 +1,22 @@
-import '../../sass/pages/home.scss';
+import { ArticleCard } from '../../components/common/ArticleCard';
+import '../../sass/pages/articles.scss';
 
 export default function News(): JSX.Element {
     return (
-        <section className='home-page'>
-            <h1>News</h1>
+        <section className='article-page'>
+            <div className="article-page__header">
+                <h1>News</h1>
+            </div>
+            <div className="article-page__body">
+                <div className="article-page__body--cards">
+                    <ArticleCard />
+                    <ArticleCard />
+                    <ArticleCard />
+                    <ArticleCard />
+                    <ArticleCard />
+                    <ArticleCard />
+                </div>
+            </div>
         </section>
     );
 }
